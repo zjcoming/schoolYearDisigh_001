835c9917e710f77c9c8de46d9db742761d483f87
ZJcoming <zjcoming0413@163.com>
2020-09-13 20:48:57 +0800

    1.完成验证码登录并自动跳转 2.完成bilibili页面布局

---
+++ app/src/main/java/com/example/test_demo/BilibiliActivity.kt
@@ -0,0 +1,11 @@
+package com.example.test_demo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BilibiliActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bilibili)
+    }
+}
