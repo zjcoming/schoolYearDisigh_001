9160b2f132db4ba3e814711098054e70d8841c7e
ZJcoming <zjcoming0413@163.com>
2020-09-15 10:50:03 +0800

    4.添加动画

--- app/src/main/java/com/example/test_demo/MainActivityZj.kt
+++ app/src/main/java/com/example/test_demo/MainActivityZj.kt
@@ -1,11 +1,25 @@
 package com.example.test_demo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.view.animation.AnimationUtils.loadAnimation
+import com.example.day13phonelogin.MainActivity
+import kotlinx.android.synthetic.main.activity_main_zj.*
 
 class MainActivityZj : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main_zj)
+        loadAnimation(this,R.anim.activity_set_anim).apply {
+            imageView6.startAnimation(this)
+        }
+        button2.setOnClickListener{
+            startActivity(Intent(this,MainActivity::class.java))
+        }
     }
+
 }
